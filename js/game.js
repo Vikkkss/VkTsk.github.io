7c2ea9bbe1d4fc44f726cfcf560c6ca07829cf04
Vikkkss <113378592+Vikkkss@users.noreply.github.com>
2022-12-13 23:29:05 +0700

    feat: update mineswepeer

--- js/game.js
+++ js/game.js
@@ -1,100 +1,47 @@
-const right = 1, left = 2, up = 3, down = 4
+import {getNeighbors, random_from_list} from "./helper.js"
 
-function findZero(state)
+
+export default class Game
 {
-    for(let l = 0; l < 3; l++)
-    {
-        for(let k = 0; k < 3; k++)
-            if(state[l][k] === 0)
-                return [l, k]
-    }
-}
+	constructor(dimension, nMines)
+	{
+		this.dimension = dimension;
+		this.nMines = nMines;
+		this.flags = new Set();
+		this.moves_made = new Set();
+		this.mines = new Set();
+		this.endGame = false;
+	}
 
-export function actions(state)
-{
-    const [i, j] = findZero(state)
-    if(i === 0 && j === 0)
-        return [up, left]
-    else if(i === 0 && j === 2)
-        return [up, right]
-    else if(i === 2 && j === 0)
-        return [down, left]
-    else if(i === 2 && j === 2)
-        return [down, right]
-    else if(i === 0)
-        return [up, left, right]
-    else if(i === 2)
-        return [down, left, right]
-    else if(j === 0)
-        return [up, down, left]
-    else if(j === 2)
-        return [up, down, right]
-    else
-        return [up, down, left, right]
-}
+	start()
+	{
+		//random choose mined cells
+		this.endGame = false;
+		let list = [];
+		this.mines = new Set();
+		this.moves_made = new Set();
+		this.flags = new Set();
+		for(let i = 0; i < this.dimension * this.dimension; i++) 
+			list.push(i);
+		for(let i = this.nMines; i > 0; --i)
+		{ 
+			this.mines.add(
+				random_from_list(list)
+			);
+		}
+	}
 
-export function successor(state, action)
-{
-    const newS =[state[0].slice(), state[1].slice(), state[2].slice()]
-    const [i, j] = findZero(state)
-    if(action === left)
-    {
-        newS[i][j] = state[i][j+1]
-        newS[i][j + 1] = 0
-    }
-    else if(action === right)
-    {
-        newS[i][j] = state[i][j - 1]
-        newS[i][j - 1] = 0
-    }
-    else if(action === down)
-    {
-        newS[i][j] = state[i - 1][j]
-        newS[i - 1][j] = 0
-    }
-    else if(action === up)
-    {
-        newS[i][j] = state[i + 1][j]
-        newS[i + 1][j] = state[i][j]
-    }
-    return newS
-}
+	countMines(cell)
+	{
+		let count = 0;
+		for(let elem of getNeighbors(cell, this.dimension))
+		{
+			if(this.mines.has(elem)) ++count;
+		}
+		return count;
+	}
 
-export function move(state, i, j)
-{
-    const zero = findZero(state)
-    const _actions = actions(state)
-    if(_actions.includes(right) && i === zero[0] && j === zero[1] - 1)
-        return successor(state, right)
-    else if(_actions.includes(left) && i === zero[0] && j === zero[1] + 1)
-        return successor(state, left)
-    else if(_actions.includes(up) && i === zero[0] + 1 && j === zero[1])
-        return successor(state, up)
-    else if(_actions.includes(down) && i === zero[0] - 1 && j === zero[1])
-        return successor(state, down)
-    else
-        return state
-}
+};
 
 
-export function goalTest(state)
-{
-    const goalState = [
-        [1, 2,3],
-        [4, 5, 6],
-        [7, 8, 0]]
-    return is_equal(goalState, state)
-}
 
-function is_equal(s1, s2)
-{
-    for(let i = 0; i < 3; i++)
-    {
-        for(let j = 0; j < 3; j++)
-        {
-            if(s1[i][j] !== s2[i][j])
-                return false
-        }
-    }
-    return true
-}
